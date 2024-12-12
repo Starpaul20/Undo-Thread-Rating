2c0396f3e9e6f7a3d9a4e4f4cbb28ddd297999ee
Paul Bender <paulbender@live.com>
2024-12-12 18:40:12 -0500

    Bump to version v1.3

--- Upload/inc/plugins/undorating.php
+++ Upload/inc/plugins/undorating.php
@@ -43,7 +43,7 @@ function undorating_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.2",
+		"version"			=> "1.3",
 		"codename"			=> "undorating",
 		"compatibility"		=> "18*"
 	);
